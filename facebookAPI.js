5c90fc67d8e16b1561719a55772d3ff18ee985dd
Dang585557 <sainglaing95@gmail.com>
2025-06-30 08:23:17 +0700

    Update facebookAPI.js

--- facebookAPI.js
+++ facebookAPI.js
@@ -1,23 +1,14 @@
 import axios from "axios";
-
 const token = process.env.PAGE_ACCESS_TOKEN;
-const pageID = process.env.PAGE_ID;
 const postID = process.env.POST_ID;
 
 export async function getComments() {
   const url = `https://graph.facebook.com/v18.0/${postID}/comments?access_token=${token}`;
   const res = await axios.get(url);
-  return res.data.data.map(c => ({
-    id: c.id,
-    message: c.message,
-    replied: !!c.comment_count,
-  }));
+  return res.data.data.map(c => ({ id: c.id, message: c.message, replied: !!c.comment_count }));
 }
 
 export async function replyToComment(commentID, message) {
   const url = `https://graph.facebook.com/v18.0/${commentID}/comments`;
-  await axios.post(url, {
-    message,
-    access_token: token
-  });
+  return axios.post(url, { message, access_token: token });
 }
