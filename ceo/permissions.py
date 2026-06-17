b70e2984c408f9763e4781d72cb2ab80b062a9ed
Dang585557 <sainglaing95@gmail.com>
2026-06-17 09:21:39 +0700

    Update permissions.py

--- ceo/permissions.py
+++ ceo/permissions.py
@@ -1,1 +1,25 @@
+"""
+AI-COMPANY
+ceo/permissions.py
+"""
+class Permissions:
 
+    def __init__(self):
+
+        self.admins = set()
+
+    def add_admin(self, user_id):
+
+        self.admins.add(str(user_id))
+
+    def remove_admin(self, user_id):
+
+        self.admins.discard(str(user_id))
+
+    def is_admin(self, user_id):
+
+        return str(user_id) in self.admins
+
+    def get_admins(self):
+
+        return list(self.admins)
